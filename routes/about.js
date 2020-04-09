35fcaa19a03fee30232d3bab18412fdbd43c62c3
kizlo <62038959+kizlo@users.noreply.github.com>
2020-04-09 12:46:50 -0400

    new changes

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('index.pug', { title: 'Express' });
+  res.render('about.pug', { title: 'Express' });
 });
 
 module.exports = router;
