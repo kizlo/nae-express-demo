fd34c922935f27ac89f8cc44a8a4d5b12aec2b09
kizlo <62038959+kizlo@users.noreply.github.com>
2020-05-17 17:54:45 -0400

    moved www from /bin to root

--- routes/indexhtml.js
+++ routes/indexhtml.js
@@ -4,8 +4,9 @@ var path = require('path');
 
 /* GET index.html page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./views/index.html');
-  //res.send('hi');
+    res.sendfile('./views/index.html');
+  
+  //res.send(path.join('/', 'views/index.html'));
 });
 
 module.exports = router;
